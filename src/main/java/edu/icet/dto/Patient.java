671885694876d3fb73a971bf73b0a18aebd3d37f
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-05-06 08:13:21 +0530

    get and post

---
+++ src/main/java/edu/icet/dto/Patient.java
@@ -0,0 +1,23 @@
+package edu.icet.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class Patient {
+    private Integer id;
+    private String name;
+    private String nic;
+    private String address;
+    private String bloodGroup;
+    private String category;
+    private String gender;
+    private String contact;
+    private String note;
+    private String allergies;
+
+}
